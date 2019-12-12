e5c5dd6e12d6c67a0d394e6c9e41795c17b2ff29
Rob J Hyndman <robjhyndman@gmail.com>
2019-12-12 12:19:21 +1100

    Added read_pubmed

--- rcademy/R/read.R
+++ rcademy/R/read.R
@@ -7,18 +7,21 @@ read_bib <- function(filename) {
     )
 }
 
-read_orcid <- function(id) {
-  id %>%
-    orcid_id() %>%
-    works()
+read_pubmed <- function(query) {
+  query %>%
+    easyPubMed::get_pubmed_ids() %>%
+    easyPubMed::fetch_pubmed_data(encoding = "ASCII") %>%
+    easyPubMed::table_articles_byAuth(included_authors = "all",
+                          max_chars = 100,
+                          autofill = TRUE)
 }
-
 
 read_scholar <- function(id) {
 
 }
 
-read_orcid("0000-0002-9341-7985")
-
+# Tests
+test1 <- read_orcid("0000-0002-9341-7985")
+test2 <- read_pubmed("Lucy D'Agostino McGowan[AU] OR LD McGowan[AU]")
 
 #ranking(journal,  source=c("abcd","scimagojr","core"))
