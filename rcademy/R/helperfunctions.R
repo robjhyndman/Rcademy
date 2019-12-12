d4c36226ddbe13c23318a3814138571af0d38930
Rob J Hyndman <robjhyndman@gmail.com>
2019-12-13 10:00:18 +1100

    Passing checks

--- rcademy/R/helperfunctions.R
+++ rcademy/R/helperfunctions.R
@@ -1,4 +1,3 @@
-# orcid.R
 # Helper functions to improve tables of papers
 # Some code borrowed from Adrian Barnett. https://github.com/agbarnett/helping.funders
 # Rewritten December 2019 by Rob Hyndman
@@ -8,25 +7,23 @@
 #' @importFrom stringr str_detect str_split
 #' @importFrom tibble tibble
 #' @importFrom dplyr mutate filter arrange
+
 # Get nicely formatted data for papers with a DOIs using crossref
-crossref_table <- function(dois) {
-  cdata.nonbibtex <- cr_works(dois)$data
+dois_to_papers <- function(dois) {
+  cdata.nonbibtex <- rcrossref::cr_works(dois)$data
   colnames(cdata.nonbibtex) <- tolower(colnames(cdata.nonbibtex))
 
   # Format papers
-  papers <- cdata.nonbibtex %>%
-    select(issued, created, container.title,
-           publisher, title, volume, issue, page, doi, type) %>%
-    purrr::pmap_dfr(format_paper) %>%
-    mutate(
-      title = as.character(title),
-      journal = as.character(journal),
-      year = as.numeric(as.character(year)),
-      volume = as.character(volume),
-      issue = as.character(issue),
-      pages = as.character(pages),
-      doi = as.character(doi)
-    )
+  papers <- cdata.nonbibtex[,c("issued", "created", "container.title",
+           "publisher", "title", "volume", "issue", "page", "doi", "type")]
+  papers <- purrr::pmap_dfr(papers, format_paper)
+  papers$title <- as.character(papers$title)
+  papers$journal = as.character(papers$journal)
+  papers$year = as.numeric(as.character(papers$year))
+  papers$volume = as.character(papers$volume)
+  papers$issue = as.character(papers$issue)
+  papers$pages = as.character(papers$pages)
+  papers$doi = as.character(papers$doi)
 
   return(papers)
 }
@@ -34,17 +31,18 @@ crossref_table <- function(dois) {
 
 # remove F1000 DOIs where there is second version (keep latest version)
 remove_f1000_dois <- function(dois) {
-  not.f1000 <- dois[!str_detect(string = dois, pattern = "f1000")]
-  f1000 <- dois[str_detect(string = dois, pattern = "f1000")]
+  X1 <- X2 <- X3 <- X4 <- NULL
+  not.f1000 <- dois[!stringr::str_detect(string = dois, pattern = "f1000")]
+  f1000 <- dois[stringr::str_detect(string = dois, pattern = "f1000")]
   if (length(f1000) > 0) { # only if some F1000 journals
-    split.f1000 <- str_split(f1000, pattern = "\\.", n = Inf, simplify = TRUE) # split by .
+    split.f1000 <- stringr::str_split(f1000, pattern = "\\.", n = Inf, simplify = TRUE) # split by .
     split.f1000 <- data.frame(split.f1000, stringsAsFactors = FALSE)
     split.f1000$X3 <- as.numeric(split.f1000$X3)
     split.f1000$X4 <- as.numeric(split.f1000$X4)
     split.f1000 <- dplyr::group_by(split.f1000, X3) %>%
       dplyr::arrange(X3, X4) %>%
-      filter(row_number() == n()) %>%
-      mutate(doi = paste(X1, ".", X2, ".", X3, ".", X4, sep = ""))
+      utils::tail(1) %>%
+      dplyr::mutate(doi = paste(X1, ".", X2, ".", X3, ".", X4, sep = ""))
     # concatenate back F1000 and not F1000
     dois <- c(not.f1000, split.f1000$doi)
   }
@@ -70,77 +68,78 @@ format_paper <- function(issued, created, container.title,
       if (is.na(journal) & publisher == "Cold Spring Harbor Laboratory")
         journal <- "bioRxiv"
 
-      tibble(journal = journal, title = title, year = year, volume = volume, issue = issue, pages = pages, type = type, doi = doi)
+      tibble::tibble(journal = journal, title = title, year = year, volume = volume,
+        issue = issue, pages = page, type = type, doi = doi)
 }
 
 
 # Following not yet working.
 # Idea is to add a column saying if named author is first author, last author, or neither.
 
-order_authors <- function(...) {
+# order_authors <- function(...) {
 
-  # make alternative versions of name
-  reverse <- paste(bio[[1]]$name$`family-name`$value, ", ",
-    substr(bio[[1]]$name$`given-names`$value, 1, 1), ".",
-    sep = ""
-  )
-  simple <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s0 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), " ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s1 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ".[A-Z] ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s2 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". [A-Z] ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s3 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". [A-Z]. ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s4 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ".[A-Z]. ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s5 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), " [A-Z] ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  s6 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), "[A-Z] ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  middle <- paste(bio[[1]]$name$`given-names`$value, " [A-Z]. ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  middle1 <- paste(bio[[1]]$name$`given-names`$value, " [A-Z] ",
-    bio[[1]]$name$`family-name`$value,
-    sep = ""
-  )
-  name.to.search <- tolower(c(name, reverse, simple, s0, s1, s2, s3, s4, s5, s6, middle, middle1))
-  index <- grep(paste(name.to.search, sep = "", collapse = "|"), tolower(authors[, 1])) # first column of authors
-  papers$First.author <- 0
-  papers$First.author[index] <- 1
-  # last author
-  authors.na <- authors
-  authors.na[authors.na == ""] <- NA # version with missing authors
-  last <- apply(authors.na, 1, function(x) tail(na.omit(x), 1)) # extract last authors
-  index <- grep(paste(name.to.search, sep = "", collapse = "|"), tolower(last)) #
-  papers$Last.author <- 0
-  papers$Last.author[index] <- 1
-  papers$Last.author[papers$First.author == 1] <- 0 # Single author papers are only flagged as first author papers
+#   # make alternative versions of name
+#   reverse <- paste(bio[[1]]$name$`family-name`$value, ", ",
+#     substr(bio[[1]]$name$`given-names`$value, 1, 1), ".",
+#     sep = ""
+#   )
+#   simple <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s0 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), " ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s1 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ".[A-Z] ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s2 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". [A-Z] ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s3 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ". [A-Z]. ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s4 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), ".[A-Z]. ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s5 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), " [A-Z] ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   s6 <- paste(substr(bio[[1]]$name$`given-names`$value, 1, 1), "[A-Z] ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   middle <- paste(bio[[1]]$name$`given-names`$value, " [A-Z]. ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   middle1 <- paste(bio[[1]]$name$`given-names`$value, " [A-Z] ",
+#     bio[[1]]$name$`family-name`$value,
+#     sep = ""
+#   )
+#   name.to.search <- tolower(c(name, reverse, simple, s0, s1, s2, s3, s4, s5, s6, middle, middle1))
+#   index <- grep(paste(name.to.search, sep = "", collapse = "|"), tolower(authors[, 1])) # first column of authors
+#   papers$First.author <- 0
+#   papers$First.author[index] <- 1
+#   # last author
+#   authors.na <- authors
+#   authors.na[authors.na == ""] <- NA # version with missing authors
+#   last <- apply(authors.na, 1, function(x) tail(na.omit(x), 1)) # extract last authors
+#   index <- grep(paste(name.to.search, sep = "", collapse = "|"), tolower(last)) #
+#   papers$Last.author <- 0
+#   papers$Last.author[index] <- 1
+#   papers$Last.author[papers$First.author == 1] <- 0 # Single author papers are only flagged as first author papers
 
-  # work out author order - so that it can be bolded in report
-  matches <- str_match(pattern = paste(name.to.search, sep = "", collapse = "|"), string = tolower(authors))
-  matches <- matrix(matches, nrow = nrow(papers))
-  author.order <- (is.na(matches) == F) %*% 1:ncol(matches) # which columns are not zero
+#   # work out author order - so that it can be bolded in report
+#   matches <- str_match(pattern = paste(name.to.search, sep = "", collapse = "|"), string = tolower(authors))
+#   matches <- matrix(matches, nrow = nrow(papers))
+#   author.order <- (is.na(matches) == F) %*% 1:ncol(matches) # which columns are not zero
 
-  return(author.order)
-}
+#   return(author.order)
+# }
--- rcademy/R/read.R
+++ rcademy/R/read.R
@@ -7,15 +7,14 @@
 #' @importFrom magrittr `%>%`
 #'
 NULL
+
 #' Read bibliographies
 #'
 #' Create tables of publications from bib files, or from PubMed, Orcid or Google Scholar
-
+#'
 #' @param filename The filename of a bib file (i.e., in BibTeX format)
 #'
 #' @return A tibble containing one row per publication. Columns include title, authors, year, journal, etc.
-#'
-#' @export
 #'
 #' @author Rob J Hyndman
 #' @examples
@@ -27,36 +26,34 @@ NULL
 #' mypubs <- read_scholar("vamErfkAAAAJ")
 #' mypubs <- read_orcid("0000-0002-9341-7985")
 #' }
+#'
+#' @export
+#'
 
 read_bib <- function(filename) {
-  filename %>%
-    RefManageR::ReadBib(check = FALSE) %>%
-    as_tibble() %>%
-    mutate(
-      title = stringr::str_remove_all(title, "[{}]"),
-      title = stringr::str_replace_all(title, "\\&", "and"),
-      journal = stringr::str_replace_all(journal, "\\\\&", "and")
+  df <- RefManageR::ReadBib(filename, check = FALSE)
+  df <- dplyr::mutate(df,
+      title = stringr::str_remove_all(df$title, "[{}]"),
+      title = stringr::str_replace_all(df$title, "\\&", "and"),
+      journal = stringr::str_replace_all(df$journal, "\\\\&", "and")
     )
 }
 
-#' @export
 #' @rdname read_bib
 #' @param query A character string containing a search query to pass to PubMed
+#' @export
 #'
 
 read_pubmed <- function(query) {
-  query %>%
+  df <- query %>%
     easyPubMed::get_pubmed_ids() %>%
     easyPubMed::fetch_pubmed_data(encoding = "ASCII") %>%
     easyPubMed::table_articles_byAuth(
       included_authors = "first",
       max_chars = 0,
       autofill = FALSE
-    ) %>%
-    as_tibble() %>%
-    pull(doi) %>%
-    unique() %>%
-    crossref_table()
+    )
+  dois_to_papers(unique(df$doi))
 }
 
 # read_scholar <- function(user) {
@@ -69,36 +66,32 @@ read_pubmed <- function(query) {
 #     as_tibble()
 # }
 
-
-#' @export
 #' @rdname read_bib
 #' @param id A character string specifying the Google Scholar ID or Orcid ID
+#' @export
 
 read_scholar <- function(id) {
-  scholar::get_publications(id) %>%
-    dplyr::mutate(
-      author = author %>% as.character() %>% stringr::str_trim(),
-    )
+  df <- scholar::get_publications(id)
+  df$author <- stringr::str_trim(as.character(df$author))
+  df
 }
 
-#' @export
 #' @rdname read_bib
+#' @export
 
 read_orcid <- function(id) {
 
   # Read works from orcid and store as a tibble
-  d <- works(orcid_id(orcid = id))
+  d <- rorcid::works(rorcid::orcid_id(orcid = id))
   if (nrow(d) == 0) {
     return(d)
   }
 
   # Get DOIs
-  dois <- identifiers(d, type = "doi") # get DOIs, not available for all papers
+  dois <- rorcid::identifiers(d, type = "doi") # get DOIs, not available for all papers
   dois <- unique(tolower(dois))
   #  dois <- dois[duplicated(tolower(dois)) == FALSE] # remove duplicates
   dois <- remove_f1000_dois(dois)
 
-  crossref_table(dois)
+  dois_to_papers(dois)
 }
-
-
