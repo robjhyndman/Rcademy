f256ea0b212c32ca990766f4bd00bc507df7fadc
Rob J Hyndman <robjhyndman@gmail.com>
2019-12-12 16:21:25 +1100

    Updated read_scholar

--- rcademy/R/read.R
+++ rcademy/R/read.R
@@ -1,6 +1,8 @@
 # Functions to read bibliographies
 
-read_bib <- function(filename, check=FALSE) {
+
+
+read_bib <- function(filename, check = FALSE) {
   filename %>%
     RefManageR::ReadBib(check = check) %>%
     as_tibble() %>%
@@ -15,29 +17,37 @@ read_pubmed <- function(query) {
   query %>%
     easyPubMed::get_pubmed_ids() %>%
     easyPubMed::fetch_pubmed_data(encoding = "ASCII") %>%
-    easyPubMed::table_articles_byAuth(included_authors = "first",
-                          max_chars = 0,
-                          autofill = FALSE) %>%
+    easyPubMed::table_articles_byAuth(
+      included_authors = "first",
+      max_chars = 0,
+      autofill = FALSE
+    ) %>%
     as_tibble() %>%
     pull(doi) %>%
     unique() %>%
     crossref_table()
 }
 
+# read_scholar <- function(user) {
+#   df <- gcite::gcite_user_info(user = user, secure = FALSE)$paper_df
+#   colnames(df)[2] <- "date"
+#   df %>%
+#     mutate(
+#       year = lubridate::year(anytime::anydate(date))
+#     ) %>%
+#     as_tibble()
+# }
+
 read_scholar <- function(user) {
-  df <- gcite::gcite_user_info(user = user, secure = FALSE)$paper_df
-  colnames(df)[2] <- "date"
-  df %>%
-    mutate(
-      year = lubridate::year(anytime::anydate(date))
-    ) %>%
-    as_tibble()
+  scholar::get_publications("GqZm90IAAAAJ") %>%
+    dplyr::mutate(
+      author = author %>% as.character() %>% stringr::str_trim(),
+    )
 }
 
 # Tests
 test1 <- read_bib("data-raw/rjhpubs.bib")
-#test2 <- read_orcid("0000-0002-2140-5352")
+# test2 <- read_orcid("0000-0002-2140-5352")
 test2 <- read_orcid("0000-0002-9341-7985")
 test3 <- read_pubmed("Rob Hyndman[AU] OR RJ Hyndman[AU]")
 test4 <- read_scholar("uERvKpYAAAAJ")
-
