e3e77d5fa5f3325d74b7d364a7a0a190d09bb560
Rob J Hyndman <robjhyndman@gmail.com>
2019-12-13 11:33:41 +1100

    Using read_csv

--- rcademy/data-raw/get_csv.R
+++ rcademy/data-raw/get_csv.R
@@ -2,13 +2,13 @@
 # setwd("~/Documents/4. R scripts/Rcademy/Rcademy/rcademy/data-raw")
 
 # read core
-core <- read.csv("CORE.csv", stringsAsFactors = FALSE, header = FALSE)
+core <- readr::read_csv("CORE.csv", col_names = FALSE)
 core <- core [,c(2, 5)]
 # add heading to core
 colnames(core) <- c("journal", "rank")
 
 # read abdc
-abdc <- read_xlsx("abdc_jql_2019_0612-1.xlsx", col_names = FALSE)
+abdc <- readxl::read_xlsx("abdc_jql_2019_0612-1.xlsx", col_names = FALSE)
 abdc <- abdc[-c(1),]
 
 # turn first row into column name
@@ -17,7 +17,7 @@ abdc <- abdc[c(-1),]
 
 # change column names in abdc
 colnames(abdc)[1] <- "journal"
-colnames(abdc)[7] <- "rank" 
+colnames(abdc)[7] <- "rank"
 
 # save into rcademy
 usethis::use_data(core, abdc, overwrite = TRUE)
