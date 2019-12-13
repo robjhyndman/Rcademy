a9dd020ee339945b0803571da51a10f81351a559
Rob J Hyndman <robjhyndman@gmail.com>
2019-12-13 12:57:51 +1100

    Documented ranking

--- rcademy/R/ranking.R
+++ rcademy/R/ranking.R
@@ -1,26 +1,44 @@
 # Combine list of publications against journal rankings
 # Options for ABDC, CORE and SCIMAGOJR
 
+#' Find rankings of journals from Scimago, Core or ABDC. Fuzzy matching is used to
+#' find
+#'
+#' @param journal A character vector containing journal names.
+#'
+#' @return A character vector of the same length as `journal` containing rankings.
+#'
+#' @author Rob J Hyndman
+#' @examples
+#'
+#' \dontrun{
+#' mypubs <- read_pubmed("Rob Hyndman")
+#' mypubs <- mypubs %>%
+#'   mutate(
+#'     abdc_ranking = ranking(journal, source="abdc"),
+#'     core_ranking = ranking(journal, source="core"),
+#'     scimago_ranking = ranking(journal, source="scimago")
+#'   )
+#' }
+#'
+#' @export
+#'
+
 ranking <- function(journal, source=c("scimagojr","abdc","core")) {
   source <- match.arg(source)
   if(source=='abdc')
     jrankings <- abdc
   else if(source=='core')
     jrankings <- core
-  else if(source=='scimagojr') {
-    jrankings <- sjrdata::sjr_journals
-    colnames(jrankings)[4] <- "journal"
-    jrankings$rank <- jrankings$sjr_best_quartile
-  } else
+  else if(source=='scimagojr')
+    jrankings <- scimagojr
+  else
     stop("Unknown rankings")
 
-  mydf <- tibble::tibble(journal=journal, ranking="")
+  mydf <- tibble::tibble(journal=journal, ranking=NA_character_)
   miss <- is.na(mydf$journal)
   fix <- fuzzyjoin::stringdist_left_join(mydf[!miss,], jrankings, by='journal')
   mydf$ranking[!miss] <- fix$rank
   return(mydf$ranking)
 }
 
-# ranking(df$journal, source='abdc')
-# ranking(df$journal, source='core')
-# ranking(df$journal, source='sci')
