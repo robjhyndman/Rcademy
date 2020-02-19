d87dd8366116e5aa5d4c55aeb231e5c207b70563
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-02-19 16:11:03 +1100

    add documentation for core

---
+++ R/data-core.R
@@ -0,0 +1,50 @@
+#' CORE (Computing Research and Education) list of conference rankings
+#'
+#' A dataset, `core` is provided, which contains the list of conference rankings
+#'   according to the CORE executive committee. It is mostly used in
+#'   the function, [ranking()]. The details of the CORE organisation,
+#'   and its procedure for ranking is provided below.
+#'
+#' CORE is an association of university departments of computer science in
+#'   Australia and New Zealand. Prior to 2004 it was known as the Computer
+#'   Science Association, CSA.
+#'
+#' The CORE Conference Ranking provides assessments of major conferences in the
+#'   computing disciplines. The rankings are managed by the CORE Executive
+#'   Committee, with periodic rounds for submission of requests for addition or
+#'   reranking of conferences. Decisions are made by academic committees based
+#'   on objective data requested as part of the submission process. Conference
+#'   rankings are determined by a mix of indicators, including citation rates,
+#'   paper submission and acceptance rates, and the visibility and research
+#'   track record of the key people hosting the conference and managing its
+#'   technical program. A more detailed statement categorizing the ranks A*, A,
+#'   B, and C can be found [here](https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnx3d3djb3JlZWR1fGd4OjJjNjkxOWE1NWQ4ZGY5MjU).
+#'
+#' Format: A data frame with 1630 observations and two variables:
+#' \itemize{
+#'   \item{`conference:`}{ Character with all}
+#'   \item{`rank:`}{ Conferences are assigned to one of the following categories:
+#'     \itemize{
+#'      \item{A*: flagship conference, a leading venue in a discipline area}
+#'      \item{A: excellent conference, and highly respected in a discipline area}
+#'      \item{B: good conference, and well regarded in a discipline area}
+#'      \item{C: other ranked conference venues that meet minimum standards}
+#'     \item{Australasian: A conference for which the audience is primarily
+#'         Australians and New Zealanders}
+#'     \item{Unranked: A conference for which no ranking decision has been made}
+#'     \item{National: A conference which is run primarily in a single country,
+#'       with Chairs from that country, and which is not sufficiently well
+#'       known to be ranked. (Papers and PC may be international}
+#'     \item{Regional: Similar to National but may cover a region crossing
+#'           national borders.}
+#'     }
+#'   }
+#'   }
+#' @name core
+#' @docType data
+#' @usage data(core)
+#' @source \url{http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CORE2018&sort=atitle&page=1}
+#' @keywords datasets
+#' @examples
+#' core
+"core"
--- data-raw/core.R
+++ data-raw/core.R
@@ -1,14 +1,21 @@
+# file downloaded from http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CORE2018&sort=atitle&page=1
+library(tidyverse)
+library(here)
+library(stringr)
+
+# function to remove whitespace, replace linebreaks and replace certain strings
+str_trim_linebreak_etc <- function(x){
+  stringr::str_trim(x) %>%
+    stringr::str_replace_all("\\n","") %>%
+    stringr::str_replace("IFIP\\?","IFIP ")
+}
+
 # read core
-core <- readr::read_csv("CORE.csv", col_names = FALSE)
-core <- core [,c(2, 5)]
-
-# add heading to core
-colnames(core) <- c("journal", "rank")
-
-# remove whitespace
-core$journal <- stringr::str_trim(core$journal)
-core$journal <- stringr::str_replace_all(core$journal,"\\n","")
-core$journal <- stringr::str_replace(core$journal, "IFIP\\?","IFIP ")
+core <- read_csv(here("data-raw", "CORE.csv"),
+                 col_names = FALSE) %>%
+  select(conference = X2,
+         rank = X5) %>%
+  mutate(conference = str_trim_linebreak_etc(conference))
 
 # save into rcademy
 usethis::use_data(core, overwrite = TRUE)
