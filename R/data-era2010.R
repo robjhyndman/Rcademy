ce2ea204fdf89f3f25b7198f26df1fa71389866f
Rob J Hyndman <robjhyndman@gmail.com>
2021-11-23 16:21:03 +1100

    Added ERA2010

---
+++ R/data-era2010.R
@@ -0,0 +1,25 @@
+#' ERA2010 Journal List
+#'
+#' This is a dataset that contains the list of journal rankings from
+#' the ARC Excellence in Research for Australia 2010 round.
+#'
+#' Format: a data frame with 20712 rows and the following 4 variables:
+#' \itemize{
+#'   \item{`eraid`: }{ERA ID of the journal}
+#'   \item{`journal`: }{Title of the journal}
+#'   \item{`issn`: }{International Standard Serial Number}
+#'   \item{`field_of_research`: }{Field of Research Code as provided by the
+#'         Australian Bureau of Statistics}
+#'   \item{`rank`: }{In order of best to lowest rank: A*, A, B, or C}
+#'  }
+#' @name era2010
+#' @docType data
+#' @usage data(era2010)
+#' @source \url{https://www.righttoknow.org.au/request/journal_list_relating_to_the_201}
+#' @keywords datasets
+#' @examples
+#' library(dplyr)
+#' era2010 %>%
+#'   filter(field_of_research == "0104") %>%
+#'   arrange(rank)
+"era2010"
