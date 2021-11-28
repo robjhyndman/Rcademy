9d960610ae4d682fc9cdac9c831aac97392cd7d2
Rob J Hyndman <robjhyndman@gmail.com>
2021-11-28 17:20:24 +1100

    Improved core raw data docs

--- data-raw/core.R
+++ data-raw/core.R
@@ -1,4 +1,3 @@
-# file downloaded from http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CORE2021&sort=atitle&page=1
 library(tidyverse)
 library(here)
 library(stringr)
@@ -10,7 +9,8 @@ str_trim_linebreak_etc <- function(x) {
     stringr::str_replace("IFIP\\?", "IFIP ")
 }
 
-# read core
+# Read core conference rankings
+# file downloaded from http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CORE2021&sort=atitle&page=1
 core <- here("data-raw", "CORE.csv") %>%
   read_csv(col_names = FALSE) %>%
   select(conference = X2, rank = X5) %>%
@@ -20,6 +20,8 @@ core <- here("data-raw", "CORE.csv") %>%
   ) %>%
   arrange(rank, conference)
 
+# Read core journal rankings
+# file downloaded from http://portal.core.edu.au/jnl-ranks/?search=&by=all&source=CORE2020&sort=atitle&page=1
 core_journals <- here("data-raw", "CORE_journals.csv") %>%
   read_csv() %>%
   rename(journal = title, field_of_research=for1, issn = ISSN1) %>%
