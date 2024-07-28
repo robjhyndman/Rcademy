48f2302b6cb2105279102b8f5f39a9a133796a8e
Rob Hyndman <robjhyndman@gmail.com>
2024-07-29 09:42:13 +1000

    Updated ABDC

--- data-raw/abdc.R
+++ data-raw/abdc.R
@@ -2,12 +2,12 @@ library(readxl)
 library(here)
 library(dplyr)
 
-# data downloaded from https://abdc.edu.au/research/abdc-journal-list
-abdc <- here("data-raw","abdc_jql_2019_0612-1.xlsx") |>
-  read_xlsx(skip = 7, .name_repair = janitor::make_clean_names) |>
+# data downloaded from https://abdc.edu.au/abdc-journal-quality-list/
+abdc <- here("data-raw","ABDC-JQL-2022-v3-100523.xlsx") |>
+  read_xlsx(skip = 8, .name_repair = janitor::make_clean_names) |>
   rename(
     title = journal_title,
-    rank = x2019_rating
+    rank = x2022_rating
   ) |>
   mutate(rank = factor(rank, levels=c("A*","A","B","C"), ordered=TRUE)) |>
   arrange(rank, title)
